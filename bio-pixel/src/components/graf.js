a4a14c83dfcf0e63588e750f0481b676b669032d
Lucas Weber <131485876+corazon261@users.noreply.github.com>
2024-02-21 08:39:07 -0300

    Create graf.js

---
+++ bio-pixel/src/components/graf.js
@@ -0,0 +1,75 @@
+import React from "react"
+import "./GrafStyle.css"
+
+export default function Frame4() {
+  return (
+    <div className="frame-4">
+      <div className="group-1">
+        <div className="group-538">
+          <p className="biopixel">Biopixel</p>
+        </div>
+        <div className="group-112">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/wo5jppvk9rl-25%3A50?alt=media&token=5e764942-56c4-49a2-8fee-580df39f369b"
+            alt="Not Found"
+            className="vector"
+          />
+        </div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/wo5jppvk9rl-173%3A27?alt=media&token=c4f09183-3530-4a10-a4fd-d73b74e5593b"
+          alt="Not Found"
+          className="polygon-6"
+        />
+      </div>
+      <div className="group-3">
+        <div className="group-496">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/wo5jppvk9rl-25%3A47?alt=media&token=6f3e7188-c1a7-4f74-94ba-853a04532e55"
+            alt="Not Found"
+            className="vector-1"
+          />
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/wo5jppvk9rl-25%3A46?alt=media&token=af8eb7e2-bc7a-4601-a245-87e7bff578bb"
+            alt="Not Found"
+            className="image-13"
+          />
+        </div>
+        <p className="amazonas">Amazonas</p>
+      </div>
+      <div className="group-4">
+        <div className="group-915">
+          <div className="group-198">
+            <p className="peixes">Peixes</p>
+          </div>
+          <div className="group-1056">
+            <p className="anf-bios">Anfíbios</p>
+          </div>
+        </div>
+        <div className="group-846">
+          <div className="group-423">
+            <p className="moluscos">Moluscos</p>
+          </div>
+          <div className="group-165">
+            <p className="rpteis">Répteis</p>
+          </div>
+        </div>
+        <div className="group-204">
+          <div className="group-758">
+            <p className="aves">Aves</p>
+          </div>
+          <div className="group-1009">
+            <p className="artropodes">Artropodes</p>
+          </div>
+        </div>
+        <div className="group-279">
+          <div className="group-215">
+            <p className="mam-feros">Mamíferos</p>
+          </div>
+          <div className="group-363">
+            <p className="anel-deos">Anelídeos</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
