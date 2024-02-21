570151d381ccbe543b45045598a28836f8cf18f0
Lucas Weber <131485876+corazon261@users.noreply.github.com>
2024-02-21 09:50:51 -0300

    Create MainGrafic.js

---
+++ bio-pixel/src/components/MainGrafic.js
@@ -0,0 +1,156 @@
+import React from "react"
+import "./styles.css"
+
+export default function Frame3() {
+  return (
+    <div className="frame-3">
+      <div className="group-5">
+        <div className="group-291">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-13%3A75?alt=media&token=0c5f0b8c-90bc-4072-ac3d-903868219e78"
+            alt="Not Found"
+            className="vector"
+          />
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-13%3A63?alt=media&token=5dd96fe6-93cc-448f-a23c-27dcc2a78674"
+            alt="Not Found"
+            className="image-7"
+          />
+        </div>
+        <p className="amazonas">Amazonas</p>
+      </div>
+      <div className="group-12">
+        <div className="group-340">
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-13%3A73?alt=media&token=20ed7228-4792-42c2-882f-3f00fbebe9f5"
+            alt="Not Found"
+            className="vector-1"
+          />
+        </div>
+        <p className="biopixel">Biopixel</p>
+      </div>
+      <div className="group-6">
+        <div className="group-393">
+          <p className="anos-mais-avistados">Anos mais avistados</p>
+          <div className="group-349">
+            <div className="ellipse-2" />
+          </div>
+        </div>
+        <img
+          src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-173%3A50?alt=media&token=dddc6272-59cb-4655-bc4a-66658e48e033"
+          alt="Not Found"
+          className="vector-2"
+        />
+      </div>
+      <div className="group-10">
+        <div className="group-649">
+          <p className="filtro">Filtro</p>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-25%3A60?alt=media&token=be9e1b99-7fa2-47a5-8bef-3893be71cd4d"
+            alt="Not Found"
+            className="image-14"
+          />
+        </div>
+        <div className="group-254">
+          <div className="rectangle-51" />
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-173%3A30?alt=media&token=d4c7f509-17b8-4015-b4aa-30acc4950907"
+            alt="Not Found"
+            className="vector-3"
+          />
+          <p className="filo-ordem-fam-lia-gnero">
+            Filo Ordem Família Gênero Espécie
+          </p>
+        </div>
+      </div>
+      <div className="group-8">
+        <p className="local">LOCAL</p>
+        <div className="group-647">
+          <div className="group-952">
+            <div className="group-812">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-23%3A3?alt=media&token=ff34201a-7f0f-493f-a33d-bd2f5fe76bd5"
+                alt="Not Found"
+                className="vector-4"
+              />
+            </div>
+          </div>
+          <img
+            src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-173%3A8?alt=media&token=193a9bcd-acde-4e35-babb-3885fb3ee775"
+            alt="Not Found"
+            className="vector-5"
+          />
+        </div>
+      </div>
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-173%3A45?alt=media&token=918c6ef4-8353-4aa2-8c8a-ac97549b3ffe"
+        alt="Not Found"
+        className="vector-6"
+      />
+      <img
+        src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-262%3A95?alt=media&token=68c5ab48-cf73-42be-9755-79254b847900"
+        alt="Not Found"
+        className="group-9"
+      />
+      <div className="group-778">
+        <div className="group-4">
+          <div className="group-333">
+            <div className="group-272">
+              <p className="peixes">Peixes</p>
+            </div>
+            <div className="group-162">
+              <p className="anf-bios">Anfíbios</p>
+            </div>
+          </div>
+          <div className="group-378">
+            <div className="group-5102">
+              <p className="moluscos">Moluscos</p>
+            </div>
+            <div className="group-214">
+              <p className="rpteis">Répteis</p>
+            </div>
+          </div>
+          <div className="group-752">
+            <div className="group-7410">
+              <p className="aves">Aves</p>
+            </div>
+            <div className="group-063">
+              <p className="artropodes">Artropodes</p>
+            </div>
+          </div>
+          <div className="group-8410">
+            <div className="group-239">
+              <p className="mam-feros">Mamíferos</p>
+            </div>
+            <div className="group-266">
+              <p className="anel-deos">Anelídeos</p>
+            </div>
+          </div>
+        </div>
+        <div className="group-7">
+          <p className="avistamentos">AVISTAMENTOS</p>
+          <div className="group-782">
+            <div className="group-757">
+              <img
+                src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-13%3A111?alt=media&token=66c63afb-120a-4437-abff-4922abb19730"
+                alt="Not Found"
+                className="vector-7"
+              />
+            </div>
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-13%3A112?alt=media&token=ce10d4be-fd9d-4493-9397-0ff770bc0ab4"
+              alt="Not Found"
+              className="vector-8"
+            />
+            <img
+              src="https://firebasestorage.googleapis.com/v0/b/unify-v3-copy.appspot.com/o/3jjxsdlb14j-173%3A7?alt=media&token=5e2f570d-e362-4f9c-85d5-1ae4f707e65e"
+              alt="Not Found"
+              className="vector-9"
+            />
+          </div>
+        </div>
+        <div className="group-118"></div>
+      </div>
+    </div>
+  )
+}
